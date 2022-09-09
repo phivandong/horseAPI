87d48757e1ed915c37a50d4a1b9c9f5b41ce49d2
graypackidz <phivandonghd@gmail.com>
2022-09-09 18:04:27 +0700

    modify some tests

---
+++ src/test/java/com/pvdong/lesson3/hibernate/HibernateUtil.java
@@ -0,0 +1,17 @@
+//package com.pvdong.lesson3.hibernate;
+//
+//import org.hibernate.SessionFactory;
+//import org.hibernate.boot.MetadataSources;
+//import org.hibernate.boot.registry.StandardServiceRegistry;
+//import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+//
+//public class HibernateUtil {
+//    public static SessionFactory getSessionFactory() {
+//        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+//                .configure()
+//                .build();
+//
+//        return new MetadataSources(registry)
+//                .buildMetadata().buildSessionFactory();
+//    }
+//}
